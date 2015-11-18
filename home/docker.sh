7dc208054048a78374f6553898e78f563b2a565f
Morgante Pell <morgante.pell@morgante.net>
2015-11-18 23:10:46 +0400

    make docker script smarter

--- home/docker.sh
+++ home/docker.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+#
+# Credit to https://gist.github.com/ladyrassilon/31fa6877a9c24ae4e8f0
 
-docker-machine start default
-eval "$(docker-machine env default)"
+docker_running=$(docker-machine ls | grep default)
+if [[ "$docker_running" == *"Stopped"* ]]
+then
+	echo "fyi - docker not running"
+elif [[ "$docker_running" == *"Running"* ]]
+then
+	eval "$(docker-machine env default)"
+fi
